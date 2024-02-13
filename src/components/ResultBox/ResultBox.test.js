03498b97e094c0e53b29b27eac4d45e07a3537cf
Anna Powaska <annapowaska@op.pl>
2024-02-13 08:53:04 +0100

    Tests split into 4 by currency

--- src/components/ResultBox/ResultBox.test.js
+++ src/components/ResultBox/ResultBox.test.js
@@ -11,12 +11,8 @@ import { cleanup} from '@testing-library/react';
     it('should render proper info about conversion when PLN -> USD', () => {
         const testCases = [
             { amount: '100', from: 'PLN', to: 'USD', result: 'PLN 100.00 = $28.57'},
-            { amount: '20', from: 'USD', to: 'PLN', result: '$20.00 = PLN 70' },
             { amount: '200', from: 'PLN', to: 'USD', result: 'PLN 200.00 = $57.14' },
-            { amount: '1', from: 'PLN', to: 'USD', result: 'PLN 1.00 = $0.29' },
-            { amount: '300', from: 'USD', to: 'PLN', result: '$300.00 = PLN 1,050.00'},
-            { amount: '300', from: 'USD', to: 'USD', result: '$300.00 = $300.00'},
-            { amount: '300', from: 'PLN', to: 'PLN', result: 'PLN 300.00 = PLN 300.00'},
+            { amount: '1', from: 'PLN', to: 'USD', result: 'PLN 1.00 = $0.29' },      
       ];
 
       for(const testObj of testCases) {
@@ -27,6 +23,37 @@ import { cleanup} from '@testing-library/react';
         cleanup();
       }
     })
+
+    it('should render proper info about conversion when USD -> PLN', () => {
+        const testCases = [
+            { amount: '20', from: 'USD', to: 'PLN', result: '$20.00 = PLN 70' },
+            { amount: '300', from: 'USD', to: 'PLN', result: '$300.00 = PLN 1,050.00'},
+      ];
+
+      for(const testObj of testCases) {
+        
+        render(<ResultBox from={testObj.from} to={testObj.to} amount={parseInt(testObj.amount)} />);
+        const output = screen.getByTestId('test');
+        expect(output).toHaveTextContent(testObj.result);
+        cleanup();
+      }
+    })
+
+    it('should render proper info about conversion when same currency', () => {
+        const testCases = [
+          { amount: '300', from: 'USD', to: 'USD', result: '$300.00 = $300.00'},
+          { amount: '300', from: 'PLN', to: 'PLN', result: 'PLN 300.00 = PLN 300.00'},
+      ];
+
+      for(const testObj of testCases) {
+        
+        render(<ResultBox from={testObj.from} to={testObj.to} amount={parseInt(testObj.amount)} />);
+        const output = screen.getByTestId('test');
+        expect(output).toHaveTextContent(testObj.result);
+        cleanup();
+      }
+    })
+
 });
 
 
